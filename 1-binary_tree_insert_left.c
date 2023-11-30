df5306d3fe2410aa9d5869b2962a042461870037
masmiyen <masmiyen@gmail.com>
2023-11-30 20:11:56 +0100

    another fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,26 +6,28 @@
  * @value: value to put in the new node
  *
  * Return: pointer to the new node, or NULLif failed
- */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+ */binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+    if (!parent) {
+        return (NULL);
+    }
 
-	if (!parent)
-		return (NULL);
+    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+    if (!newNode) {
+        return NULL;
+    }
 
-	if (!newNode)
-		return (NULL);
+    newNode->parent = NULL;
+    newNode->n = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
-	newNode->parent = parent;
-	newNode->n = value;
-	newNode->left = parent->left;
-	newNode->right = NULL;
+    if (parent->left) {
+        parent->left->parent = newNode;
+    }
+    newNode->left = parent->left;
+    parent->left = newNode;
+    newNode->parent = parent;
 
-	if (parent->left)
-		parent->left->parent = newNode;
-
-	parent->left = newNode;
-
-	return (newNode);
+    return (newNode);
 }
