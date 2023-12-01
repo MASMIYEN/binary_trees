a07d0064fd72822cec5f2c22bca883ed91f9f5ae
0ussamabernou <oussama.bernou@outlook.com>
2023-12-01 01:20:30 +0100

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
-size_t depth(const binary_tree_t *node);
+size_t max_depth(const binary_tree_t *node);
 
 /**
  * is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
- * @d: depth of the tree
+ * @d: maximum depth of the tree
  * @level: level of the tree
  *
  * Return: 1 if tree is perfect, otherwise 0
@@ -16,7 +16,7 @@ int is_perfect(const binary_tree_t *tree, int d, int level)
 		return (1);
 
 	if (!tree->left && !tree->right)
-		return (d == level + 1);
+		return (d == level);
 
 	if (!tree->left || !tree->right)
 		return (0);
@@ -33,29 +33,23 @@ int is_perfect(const binary_tree_t *tree, int d, int level)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int d = depth(tree);
+	int d = max_depth(tree);
 
-	return (is_perfect(tree, d, 0));
+	return (is_perfect(tree, d, 1));
 }
 
 /**
- * depth - measures the depth of a node in a binary tree
+ * max_depth - measures the maximum depth of a node in a binary tree
  * @node: pointer to the node to measure the depth
  *
- * Return: depth of the node, or 0 if tree is NULL
+ * Return: maximum depth of the node
  */
-size_t depth(const binary_tree_t *node)
+size_t max_depth(const binary_tree_t *node)
 {
-	size_t depth = 0;
-
-	if (node == NULL)
+	if (!node)
 		return (0);
 
-	while (node)
-	{
-		depth++;
-		node = node->left;
-	}
-
-	return (depth);
+	return (1 + max_depth(node->left) > 1 + max_depth(node->right)
+		    ? 1 + max_depth(node->left)
+		    : 1 + max_depth(node->right));
 }
