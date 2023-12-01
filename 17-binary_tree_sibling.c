9fa1cb6aed1e5db6d528a33f5149d6f154289898
0ussamabernou <oussama.bernou@outlook.com>
2023-12-01 01:35:58 +0100

    update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,11 +14,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		if (node->parent->left == node)
 		{
-			return (node->right);
+			return (node->parent->right);
 		}
 		else
 		{
-			return (node->parent->right);
+			return (node->parent->left);
 		}
 	}
 	else
