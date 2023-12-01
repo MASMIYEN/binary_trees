fa61270393add43d19f3a768980ec73545af8a83
0ussamabernou <oussama.bernou@outlook.com>
2023-12-01 01:14:52 +0100

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,14 +3,16 @@
 size_t depth(const binary_tree_t *node);
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
+ * is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
+ * @d: depth of the tree
+ * @level: level of the tree
  *
  * Return: 1 if tree is perfect, otherwise 0
  */
 int is_perfect(const binary_tree_t *tree, int d, int level)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (1);
 
 	if (!tree->left && !tree->right)
@@ -23,14 +25,22 @@ int is_perfect(const binary_tree_t *tree, int d, int level)
 		is_perfect(tree->right, d, level + 1));
 }
 
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is perfect, otherwise 0
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int d = depth(tree);
-	return is_perfect(tree, d, 0);
+
+	return (is_perfect(tree, d, 0));
 }
+
 /**
- *depth - measures the depth of a node in a binary tree
- * @tree: pointer to the node to measure the depth
+ * depth - measures the depth of a node in a binary tree
+ * @node: pointer to the node to measure the depth
  *
  * Return: depth of the node, or 0 if tree is NULL
  */
