7ddbd50fc4e8791601e062e86db71e6e228a3982
masmiyen <masmiyen@gmail.com>
2023-12-02 05:54:42 +0100

    just swapped left with right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_rotate_left - do left rotation of the tree
+ *
+ * @tree: the binary tree
+ *
+ * Return: the new root of the tree
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	tree->parent = tree->left;
+
+	if (tree->left)
+	{
+		tree->left = tree->left->right;
+		tree->parent->right = tree;
+		tree->parent->parent = NULL;
+
+		if (tree->left)
+			tree->left->parent = tree;
+		return (tree->parent);
+	}
+	return (tree);
+}
